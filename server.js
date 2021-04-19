4881e90898799785c576ce3894d8b4e886ef524d
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-19 21:00:28 +0530

    adding quiz attempt controller server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(function (req, res, next) {
 
 require('./controllers/quizzes-controller')(app)
 require('./controllers/question-controller')(app)
+require('./controllers/quiz-attempts-controller')(app)
 
 //app.listen(4000)
 app.listen(process.env.PORT || 4000)
