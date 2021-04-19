1f4e8c502d7f654b2b2ab7f6f3de3756ba0bdb9d
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-20 01:27:18 +0530

    removing _id for auto _id in quiz attempts

--- models/quiz-attempts/quiz-attempts-schema.js
+++ models/quiz-attempts/quiz-attempts-schema.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const questionSchema = require('../questions/questions-schema')
 
 const quizAttempts = mongoose.Schema({
-    _id: String,
     score: Number,
     quiz: {type: String, ref: 'QuizzesModel'},
     answers: [questionSchema]
